d280ed72bfb42e67821c70b832bcba7ff986a669
Dan Schultz <schultz.t.dan@gmail.com>
2020-03-20 21:17:09 -0500

    Add CI script (#2)
    * Add CI script
    
    * Add coverage threshold
    
    * Add build badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # zen-3d-addons
+
+![CI Build](https://github.com/danschultz/zen-3d-addons/workflows/CI%20Build/badge.svg)
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@
 
 module.exports = {
   coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
